5f81385acb5944da167c83ee5c11228a19288e5b
ruwinib <ruwinibhaggya@gmail.com>
2019-02-21 01:05:59 +0530

    edited the result page

--- eo/assets/js/index.js
+++ eo/assets/js/index.js
@@ -3,7 +3,42 @@ function calculateFinalMarks(valuesObj) {
   $.each(valuesObj, function(key, value) {
     total += value;
   });
-  return (total - 24) * 100 / 96;
+  var final = ((total - 24) * 100) / 96;
+  return final.toFixed(2);
+}
+
+function drawGraph(valuesObj, selector) {
+  var total = 0;
+  var lablesVals = [
+    "Pro-activeness",
+    "Risk taking",
+    "Autonomy",
+    "Innovativeness",
+    "Competitive Aggressiveness"
+  ];
+  var marksVals = [0, 0, 0, 0, 0];
+  var quectionCountVals = [5, 5, 3, 7, 4];
+  var totalVals = [20, 20, 12, 28, 16];
+  var seriesVals = [0, 0, 0, 0, 0];
+  $.each(valuesObj, function(key, value) {
+    var index = parseInt(key.charAt(2)) - 1;
+    marksVals[index] += value;
+  });
+  for (var i = 0; i < 5; i++) {
+    seriesVals[i] =
+      ((marksVals[i] - quectionCountVals[i]) * 100) / totalVals[i];
+    seriesVals[i] = seriesVals[i].toFixed(2);
+  }
+  new Chartist.Bar(
+    selector,
+    {
+      labels: lablesVals,
+      series: seriesVals
+    },
+    {
+      distributeSeries: true
+    }
+  );
 }
 
 function init() {
@@ -21,7 +56,8 @@ function init() {
             title:
               "Being uneducated and specifically untrained is not an obstacle for farming",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -29,7 +65,8 @@ function init() {
             title:
               "I/we have self-confidence about my abilities and competencies",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -37,7 +74,8 @@ function init() {
             title:
               "I/we act opportunistically to shape the business environment in which we operate",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -45,7 +83,8 @@ function init() {
             title:
               "I/we look forward to seize initiatives whenever possible in our target market operations",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -53,7 +92,8 @@ function init() {
             title:
               "I/We seek to exploit anticipated changes in future market conditions ahead of our rivals",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           }
         ]
       },
@@ -65,7 +105,8 @@ function init() {
             name: "q_2_1",
             title: "A recession or loss is temporary in farming",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -73,7 +114,8 @@ function init() {
             title:
               "I have no doubt to try new crop varieties and new technologies",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -81,7 +123,8 @@ function init() {
             title:
               "I'm in general, tends to invest in high-risk projects (with chances of high returns).",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -89,7 +132,8 @@ function init() {
             title:
               "My business shows a great deal of tolerance for venturing into the unknown",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -97,7 +141,8 @@ function init() {
             title:
               "My business strategy is characterized by a tendency to commit resources into projects with uncertain outcomes",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           }
         ]
       },
@@ -109,7 +154,8 @@ function init() {
             name: "q_3_1",
             title: "I like to work independently",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -117,7 +163,8 @@ function init() {
             title:
               "All plans should be challengeable according to my need and requirement",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -125,7 +172,8 @@ function init() {
             title:
               "Generally, I behave autonomously in my existing business operations",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           }
         ]
       },
@@ -138,7 +186,8 @@ function init() {
             title:
               "Problems and challenges are sold through experience and innovative thinking",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -146,7 +195,8 @@ function init() {
             title:
               "The productivity of the farm can be increased through innovation and creativity",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -154,7 +204,8 @@ function init() {
             title:
               "Although following the regular pattern is easier modifying, adopting new agricultural methods according to the local conditions is more useful",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -162,7 +213,8 @@ function init() {
             title:
               "During the past three years I have developed new fertilizer/ liquid fertilizer",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -170,7 +222,8 @@ function init() {
             title:
               "During the past three years I have developed new farming equipment/machine",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -178,7 +231,8 @@ function init() {
             title:
               "I think my business provides technological leadership in developing new products/services.",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -186,7 +240,8 @@ function init() {
             title:
               "Generally, I'm trying to provide my products with a novel idea",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           }
         ]
       },
@@ -198,7 +253,8 @@ function init() {
             name: "q_5_1",
             title: "I try hard to take customers from competitors",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -206,7 +262,8 @@ function init() {
             title:
               "I watch competitors' business strategies to react against them promptly",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
@@ -214,26 +271,37 @@ function init() {
             title:
               "I prefer aggressive marketing of new menus of growing and services through the Internet",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           },
           {
             type: "rating",
             name: "q_5_4",
             title: "I prefer aggressive price competition",
             mininumRateDescription: "completely disagree",
-            maximumRateDescription: "completely agree"
+            maximumRateDescription: "completely agree",
+            isRequired: true
           }
         ]
       }
-    ]
+    ],
+    completedHtml:
+      "<p><h2>Thank You For Using Our Application !!!</h2></p><p>Please find below your result and result destribution</p>"
   };
 
   window.survey = new Survey.Model(json);
 
-  survey.onComplete.add(function(result) {
-    document.querySelector("#surveyResult").innerHTML =
-      "result: " + calculateFinalMarks(result.data) + "%";
+  survey.onComplete.add(function(result, options) {
+    console.log(result, options);
+    document.querySelector("#final_marks").innerHTML =
+      calculateFinalMarks(result.data) + "%";
+    document.querySelector(".sv_result_heading").innerHTML =
+      "Your Score for EO";
+    drawGraph(result.data, "#final_chart");
+    $(".sv_reload").show();
   });
+
+  survey.requiredText = "";
 
   $("#surveyElement").Survey({
     model: survey
